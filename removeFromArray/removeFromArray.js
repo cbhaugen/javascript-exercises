92fa12a258efcfb1e60bbc70fce53b7440e2b2cb
Christy Haugen <cbhaugen@gmail.com>
2019-10-03 11:09:40 -0500

    pass first spec test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elem) {
+  for (var i=0; i<array.length; i++){
+    if (array.includes(3)) {
+      if (array[i] === 3) {
+        array.splice(i,1);
+        i--;
+        return array;
+      }
+    }
+  }
+
+
 
 }
 
