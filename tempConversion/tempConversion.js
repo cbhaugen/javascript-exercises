006f9f0d043aa366337f2e1fc079b5f10202469f
Christy Haugen <cbhaugen@gmail.com>
2019-10-06 14:54:19 -0500

    all tests passed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let c = (temp-32) * (5/9);
+  let cRounded = parseFloat(c.toFixed(1));
+  return cRounded;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let f = (temp * (9/5) + 32);
+  let fRounded = parseFloat(f.toFixed(1));
+  return fRounded;
 }
 
 module.exports = {
