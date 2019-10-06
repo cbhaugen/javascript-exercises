cde39cead634ee617e0e0609c2515bbb5d95d5f6
Christy Haugen <cbhaugen@gmail.com>
2019-10-05 22:45:53 -0500

    pass all tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (start>end){
+    [start,end] = [end,start];
+  }
+  if ((start<0) || (end<0)) return "ERROR";
+  if (typeof start !== "number" || typeof end !== "number") return "ERROR";
 
+  let singleVal = 0;
+
+  for(let i=start; i < end+1; i++){
+    singleVal += i;
+  }
+  return singleVal;
 }
 
+
 module.exports = sumAll
+
+/*
+- create a variable to hold the final sum
+- loop through the given numbers ([link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration))
+- on each iteration add the number to the sum
+- return the sum after finishing the loop*/
